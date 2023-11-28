976d878794d40cafc5c4d0aa33c1578618894f5b
F1eshka <144143888+F1eshka@users.noreply.github.com>
2023-11-28 22:34:55 +0300

    Update HW 24.10.cpp

--- HW 24.10.cpp
+++ HW 24.10.cpp
@@ -1,20 +1,53 @@
-﻿// HW 24.10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+using namespace std;
+ // Разворот числа
+void Sum(int a)
+{
+    if (a > 0)
+    {
+        cout << a % 10;
+        Sum(a / 10);
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a;
+    cin >> a;
+    Sum(a);
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+// Степень числа
+int degreeofnumber(int number, int degree) {
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    if (degree == 0)return 1;
+    return number * degreeofnumber(number, degree - 1);
+}
+int main() {
+    int number;
+    int degree;
+    cout << "enter a number: ";
+    cin >> number;
+    cout << "enter the degree: ";
+    cin >> degree;
+    int result = degreeofnumber(number, degree);
+    cout << result;
+}
+
+// Сумма чисел в диапазоне
+int Sum(int a, int b)
+{
+    if (a > b)return 0;
+    return a + Sum(a + 1, b);
+}
+
+int main() {
+    int a;
+    int b;
+    cout << "Enter the first number in the range: ";
+    cin >> a;
+    cout << "Enter a lower number in the range of: ";
+    cin >> b;
+    int sum = Sum(a, b);
+    cout << sum;
+}
